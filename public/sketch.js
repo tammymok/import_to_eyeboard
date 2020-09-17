76dd22aa472c1296ae1a673bbdf872bd943be55b
tmok825 <tamok@ucsd.edu>
2020-09-17 15:40:30 -0700

    trying to connect stuff

--- public/sketch.js
+++ public/sketch.js
@@ -11,7 +11,9 @@ function setup() {
   background(0);
   // Start a socket connection to the server
   // Some day we would run this server somewhere else
-  socket = io.connect('http://localhost:3000');
+  //socket = io.connect('http://localhost:3000');
+  socket = io.connect('https://eyeboard.herokuapp.com/');
+
   // We make a named event called 'mouse' and write an
   // anonymous callback function
   socket.on('mouse',
