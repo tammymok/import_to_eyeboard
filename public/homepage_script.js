3c29bea7762fc8cb5c98354d2c7dcfb7c92b60d6
tmok825 <tamok@ucsd.edu>
2020-10-06 22:51:45 -0700

    cleaned up homepage, added fullscreen option

--- public/homepage_script.js
+++ public/homepage_script.js
@@ -1,3 +1,20 @@
 var open_whiteboard = () => {
    window.location.href = './whiteboard'; 
 }
+
+function open_game() {
+    window.location.href = './game';
+}
+var elem = document.getElementsByTagName("BODY")[0];
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+} 
